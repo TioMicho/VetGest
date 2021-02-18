dd749364690ae2f2ebfdecf2ef2fc577b3aaaa08
TioMicho <megaliostiaganzosorrex@gmail.com>
2021-02-18 17:26:34 -0300

    first commit

---
+++ Models/Caso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace VetGest.Models
+{
+    public class Caso
+    {
+        [Key]
+        public Guid ID { get; set; } // cambiar por un autogenerador incremental "CasoN°"
+        public Guid PacienteID { get; set; } //id del paciente
+        public DateTime Fecha { get; set; } // fecha y hora de creacion
+
+        public string MotivoConsulta { get; set; }
+        public int? Peso { get; set; }
+        public int? Temperatura { get; set; }
+        public string EstadoCorporal { get; set; }
+        public string TiempoLlenadoCapilar { get; set; }
+        public string Hidratacion { get; set; }
+        public string FrecuenciaRespiratoria { get; set; }
+        public string FrecuenciaCardiaca { get; set; }
+        public string AllazgosClinicos { get; set; }
+        public string RecomendacionesMedicacion { get; set; }
+
+
+    }
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetGest.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public Guid ID { get; set; }
+        //datos del propietario-----
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [MaxLength(50)]
+        public string DueñoNombre { get; set; }
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [MaxLength(50)]
+        public string DueñoApellido { get; set; }
+        public string Domicilio { get; set; }
+        public int? Telefono { get; set; }
+        // datos del paciente---
+        public string Nombre { get; set; }
+        [Display(Name = "Fecha Nac.")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime? FechaNacimiento { get; set; }
+        public string Raza { get; set; }
+        public string Especie { get; set; }
+        public string Pelaje { get; set; }
+        public string Sexo { get; set; }
+        public string Chip { get; set; }
+
+
+    }
+}
